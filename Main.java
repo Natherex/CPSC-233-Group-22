0873b82e7938e9a22da7f743a5697a77796dd992
Marcus-K <kasdorfmgl@gmail.com>
2019-02-11 15:16:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        ChessBoard gameBoard = new ChessBoard();
+        gameBoard.initialize();
+
+        boolean gameOver = false;
+        boolean isInCheck = false;
+        do {
+            playerOneTurn(gameBoard);
+            isInCheck = inCheck(gameBoard);
+            gameOver = isGameOver(gameBoard);
+            if (gameOver)
+                break;
+
+            playerTwoTurn(gameBoard);
+            isInCheck = inCheck(gameBoard);
+            gameOver = isGameOver(gameBoard);
+            if (gameOver)
+                break;
+
+        } while (true);
+    }
 }
