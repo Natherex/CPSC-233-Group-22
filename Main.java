243bc4e65752fdecbbce124a8bb6bf9ae59ff081
Natherex <31608824+Natherex@users.noreply.github.com>
2019-02-13 15:36:52 -0700

    Update Main.java
    Completed Main as a group

--- Main.java
+++ Main.java
@@ -1,29 +1,51 @@
 public class Main {
     public static void main(String[] args) {
         ChessBoard board = new ChessBoard();
+        GameState state = new GameState();
+        player player1 = new player(1);
+        player player2 = new player(2);
+        
+        String pieceToMove;
+        int gameState;
         board.initialize();
-
-        boolean gameOver = false;
-        boolean isInCheck = false;
-
         /*  
         Game state 0: All Good
         Game state 1: In Check
         Game state 2: Checkmate
+        Game State 3 : StaleMate
         */ 
-        int gameState = 0;
-
         do {
-            playerOneTurn(board);
-            gameState = updateGameState(board);
-            if (gameState == 2)
+            do
+            {
+                
+                startLocation = player1.choosePiece();
+                moveToLocation = player1.placeToMove();
+                
+            }while(!isValid.isValid(board,state,startLocation,moveToLocation);
+            board.movePiece(startLocation,moveToLocation);
+            state.updateGameState(board);
+            gameState = state.getGameState();
+                   
+            if (gameState == 2 || gameState == 3)
+            {
+                //end game message
+                break;
+            }
+                   
+            do
+            {
+                
+                startLocation = player2.choosePiece();
+                moveToLocation = player2.placeToMove();
+                
+            }while(!isValid.isValid(board,state,startLocation,moveToLocation);
+            board.movePiece(startLocation,moveToLocation);       
+            state.updateGameState(board);
+            gameState = state.getGameState();
+                   
+            if (gameState == 2 || gameState == 3)
                 break;
 
-            playerTwoTurn(board);
-            isInCheck = inCheck(board);
-            gameOver = isGameOver(board);
-            if (gameOver)
-                break;
 
         } while (true);
     }
