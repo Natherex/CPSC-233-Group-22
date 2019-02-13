f582bf3e4040d58bb9d57675913bdb68e4fd403e
Marcus-K <kasdorfmgl@gmail.com>
2019-02-13 09:38:18 -0700

    Updated Board
    Added isValidLocation, in process of added movePiece

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,6 +1,10 @@
 import pieces.*;
 
 public class ChessBoard extends Board {
+    private boolean isFlipped = false;
+    private static char[] validColumns = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+    private static int[] validRows = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
     public ChessBoard() {
         super(8, 8);
     }
@@ -32,6 +36,8 @@ public class ChessBoard extends Board {
 
     }
 
+    // Flips all the coordinate system of the board.
+    // Reassigns pieces accordingly.
     public void flipBoard() {
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 8; x++) {
@@ -40,6 +46,41 @@ public class ChessBoard extends Board {
                 grid[7 - y][x] = temp;
             }
         }
+
+        isFlipped = !isFlipped;
+    }
+
+    public void movePiece() {
+
+    }
+
+    private int[] parseMove(String location) {
+        int[] coordinates = new int[2];
+
+        if (isFlipped) {
+
+        }
+
+        return new int[2];
+    }
+
+
+    private static boolean isValidLocation(String location) {
+        if (location.length() != 2)
+            return false;
+
+        char column = location.charAt(0);
+        int row = Character.getNumericValue(location.charAt(1));
+        int checks = 0;
+
+        for (int i  = 0; i < 8; i++) {
+            if (column == validColumns[i])
+                checks++;
+            if (row == validRows[i])
+                checks++;
+        }
+
+        return checks == 2;
     }
 
     public String toString() {
@@ -70,5 +111,6 @@ public class ChessBoard extends Board {
         System.out.println();
         board.flipBoard();
         System.out.println(board);
+        System.out.println(isValidLocation("!!"));
     }
 }
