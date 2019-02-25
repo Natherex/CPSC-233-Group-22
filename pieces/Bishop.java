d4b1e276243cb0513f8e98df24bc712f8861c28b
braeden-deleon <47432118+braeden-deleon@users.noreply.github.com>
2019-02-24 21:20:51 -0700

    Update Bishop.java
    Added in isValidMove function to Bishop.

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -1,5 +1,7 @@
 package pieces;
 
+import board.Chessboard;
+
 public class Bishop extends Piece {
     public Bishop() {
         super();
@@ -11,5 +13,30 @@ public class Bishop extends Piece {
 
     public String toString() {
         return "Bishop(" + getColor() + ")";
+    }
+    
+    public boolean isValidMove(ChessBoard board, String start, String end) {
+        int[] totalDistance = board.distance(start, end);
+        if (totalDistance == null)
+            return false;
+        
+        int xDirection = totalDistance[1];
+        int yDirection = totalDistance[0];
+        
+        //Can only move diagonally if clear
+        if (xDirection == yDirection && board.isNotBlocked(start, end) {
+            incrementTimesMoved();
+            return true;
+        ]
+        
+        //Kills piece if end location has an enemy piece
+        else if (xDirection == yDirection && bpard.isBlocked(start, end) {
+            incrementTimesMoved();
+            board.removePiece(end);
+            return true;
+        }
+        
+        return false;
+    
     }
 }
