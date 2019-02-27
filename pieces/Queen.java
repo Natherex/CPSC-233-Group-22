04dd4c884ee8aaf8f2c0b7b6f367b35bc5d93576
Natherex <31608824+Natherex@users.noreply.github.com>
2019-02-27 15:13:51 -0700

    Update Queen.java
    added abs

--- pieces/Queen.java
+++ pieces/Queen.java
@@ -33,25 +33,25 @@ public class Queen extends Piece {
         }
         
         //Can kill in all cardinal diections
-        else if ((xDirection == 0 || yDirection == 0) && board.isBlocked(start, end)) {
-            incrementTimesMoved();
-            board.removePiece(end);
-            return true;
+        //else if ((xDirection == 0 || yDirection == 0) && board.isBlocked(start, end)) {
+        //    incrementTimesMoved();
+        //    board.removePiece(end);
+        //    return true;
         
         }
         
         //Can move on diagonals
-        else if ((xDirection == yDirection) && board.isNotBlocked(start, end)) {
+        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isNotBlocked(start, end)) {
             incrementTimesMoved();
             return true;
           
         }
         
         //Can kill pieces on diagonals
-        else if ((xDirection == yDirection) && board.isBlocked(start, end)) {
-            incrementTimesMoved();
-            board.removePiece(end);
-            return true;
+        //else if ((xDirection == yDirection) && board.isBlocked(start, end)) {
+        //    incrementTimesMoved();
+        //    board.removePiece(end);
+        //    return true;
         
         }
     
