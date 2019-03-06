ba71656cb391cf435278bd0183ef89cdbe6eb758
kaitlynkim <47432124+kaitlynkim@users.noreply.github.com>
2019-03-05 23:32:55 -0700

    Update Rook.java
    Added image

--- pieces/Rook.java
+++ pieces/Rook.java
@@ -1,16 +1,31 @@
 package pieces;
 
 import board.*;
+import javafx.scence.image.Image;
 
 public class Rook extends Piece {
+    private Image icon;
+    
     public Rook() {
         super();
         setName("Rook");
+        
+        //If the color is white, set the icon of the piece to the white rook, otherwise it'll be the black rook.
+	    if (getColor().equals("w"))
+	        setIcon(new Image("/assets/Chess_rlt60.png"));
+	    else
+	        setIcon(new Image("/assets/Chess_rdt60.png"));
     }
 
     public Rook(String color) {
         super(color);
         setName("Rook");
+        
+        //If the color is white, set the icon of the piece to the white rook, otherwise it'll be the black rook.
+	    if (getColor().equals("w"))
+	        setIcon(new Image("/assets/Chess_rlt60.png"));
+	    else
+	        setIcon(new Image("/assets/Chess_rdt60.png"));
     }
 
     public String toString() {
