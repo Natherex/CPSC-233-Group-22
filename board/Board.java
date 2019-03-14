0d41bc65ada10ae16c378d37f17c944651b7d09a
Marcus-K <kasdorfmgl@gmail.com>
2019-03-14 15:15:07 -0600

    Update Board.java

--- board/Board.java
+++ board/Board.java
@@ -22,17 +22,20 @@ public class Board {
     }
 
     public Piece[][] getGrid() {
-//        Piece[][] tempGrid = new Piece[height][length];
-//
-//        for (int row = 0; row < height; row++) {
-//            for (int column = 0; column < length; column++) {
-//                if (grid[row][column] != null)
-//                    tempGrid[row][column] = new Piece(grid[row][column]);
-//            }
-//        }
-//
-//        return tempGrid;
         return grid;
+    }
+
+    public Piece[][] getCopyGrid() {
+        Piece[][] tempGrid = new Piece[height][length];
+
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < length; column++) {
+                if (grid[row][column] != null)
+                    tempGrid[row][column] = new Piece(grid[row][column]);
+            }
+        }
+
+        return tempGrid;
     }
 
 }
