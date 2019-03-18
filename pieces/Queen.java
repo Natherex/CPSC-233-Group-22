fac692310fbb8c1a720f5609d0e2f0400e864f5e
Natherex <31608824+Natherex@users.noreply.github.com>
2019-03-18 13:16:23 -0600

    Added commenting and updated validity of all moves

--- pieces/Queen.java
+++ pieces/Queen.java
@@ -27,7 +27,15 @@ public class Queen extends Piece {
     public String toString() {
         return "Qu(" + getColor() + ")";
     }
-    
+
+    /**
+     * Tests if move is a valid move on a given chess board.
+     * @param board Needs a chess board that the pawn is on.
+     * @param start Starting location of the piece on the chess board.
+     * @param end Ending location of the piece on the chess board.
+     * @return Returns true if the piece can make the move given,
+     *         returns false otherwise.
+     */
     public boolean isValidMove(ChessBoard board, String start, String end) {
         int[] totalDistance = board.distance(start, end);
         if (totalDistance == null)
@@ -36,22 +44,27 @@ public class Queen extends Piece {
         int xDirection = totalDistance[1];
         int yDirection = totalDistance[0];
         
+        int[] startCoordinate = board.parseLocation(start);
+        int startY = startCoordinate[0];
+        int startX = startCoordinate[1];
+
+        int[] endCoordinate = board.parseLocation(end);
+        int endY = endCoordinate[0];
+        int endX = endCoordinate[1];
+        
+        
+        String color = board.getGrid()[startY][startX].getColor();
+        
         // Can move in either cardinal direction
-        if ((Math.abs(xDirection) ==  Math.abs(yDirection)) && board.isNotBlocked(start, end) && board.isWayClear(start,end)) {
+        if ((Math.abs(xDirection) ==  Math.abs(yDirection)) && board.isNotBlocked(start, end) && board.isWayClear(start,end) && canPieceMoveLegally(board,start,end,color)) {
             incrementTimesMoved();
             return true;
         
         }
         
         // Can kill in all cardinal directions
-        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isBlocked(start, end) && board.isWayClear(start,end)) {
-            int[] startCoordinate = board.parseLocation(start);
-            int startY = startCoordinate[0];
-            int startX = startCoordinate[1];
+        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isBlocked(start, end) && board.isWayClear(start,end) && canPieceMoveLegally(board,start,end,color)) {
 
-            int[] endCoordinate = board.parseLocation(end);
-            int endY = endCoordinate[0];
-            int endX = endCoordinate[1];
             if(!board.getGrid()[startY][startX].getColor().equals(board.getGrid()[endY][endX].getColor()))
             {
             	System.out.println("test 2");
@@ -62,21 +75,15 @@ public class Queen extends Piece {
         }
 
         // Can move on diagonals
-        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isNotBlocked(start, end) && board.isWayClear(start,end)) {
+        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isNotBlocked(start, end) && board.isWayClear(start,end) && canPieceMoveLegally(board,start,end,color)) {
             incrementTimesMoved();
             return true;
           
         }
         
         // Can kill pieces on diagonals
-        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isBlocked(start, end) && board.isWayClear(start,end)) {
-            int[] startCoordinate = board.parseLocation(start);
-            int startY = startCoordinate[0];
-            int startX = startCoordinate[1];
+        else if ((Math.abs(xDirection) == Math.abs(yDirection)) && board.isBlocked(start, end) && board.isWayClear(start,end) && canPieceMoveLegally(board,start,end,color)) {
 
-            int[] endCoordinate = board.parseLocation(end);
-            int endY = endCoordinate[0];
-            int endX = endCoordinate[1];
             if(!board.getGrid()[startY][startX].getColor().equals(board.getGrid()[endY][endX].getColor()))
             {
             	System.out.println("test 2");
@@ -87,20 +94,14 @@ public class Queen extends Piece {
         }
 
         // Can move up/down/left/right
-        else if (board.isNotBlocked(start, end) && (xDirection == 0 || yDirection == 0) && board.isWayClear(start,end)) {
+        else if (board.isNotBlocked(start, end) && (xDirection == 0 || yDirection == 0) && board.isWayClear(start,end) && canPieceMoveLegally(board,start,end,color)) {
             incrementTimesMoved();
             return true;
         }
 
         // Can kill up/down/left/right
-        else if (board.isBlocked(start, end) && (xDirection == 0 || yDirection == 0) && board.isWayClear(start,end)) {
-            int[] startCoordinate = board.parseLocation(start);
-            int startY = startCoordinate[0];
-            int startX = startCoordinate[1];
+        else if (board.isBlocked(start, end) && (xDirection == 0 || yDirection == 0) && board.isWayClear(start,end) && canPieceMoveLegally(board,start,end,color)) {
 
-            int[] endCoordinate = board.parseLocation(end);
-            int endY = endCoordinate[0];
-            int endX = endCoordinate[1];
             if(!board.getGrid()[startY][startX].getColor().equals(board.getGrid()[endY][endX].getColor()))
             {
             	System.out.println("test 2");
