6bb13b8d34b2dc5a7fc62f713967ed914a84fee7
braeden-deleon <47432118+braeden-deleon@users.noreply.github.com>
2019-04-12 00:44:01 -0600

    Added in test files for all pieces

--- test_files/PawnTest.java
+++ test_files/PawnTest.java
@@ -31,4 +31,44 @@ public class PawnTest {
 
         assertFalse("White pawn moving from A2 to A4 shouldn't be possible with a black pawn at A3", board.movePiece("A2", "A4"));
     }
+
+    @Test
+    public void killPawnTest() {
+
+        ChessBoard board = new ChessBoard();
+        board.placePiece("King", "w", "A1");
+        board.placePiece("King", "b", "A8");
+        board.placePiece("Pawn", "w", "B1");
+        board.placePiece("Pawn", "b", "C2");
+        board.placePiece("Pawn", "w", "C7");
+        board.placePiece("Pawn", "b", "B8");
+
+        assertTrue("White pawn should be able to kill black pawn on C3", board.movePiece("B1", "C2"));
+
+        board.changeTurn();
+
+        assertTrue("Black pawn should be able to kill white pawn on C7", board.movePiece("B8", "C7"));
+
+    }
+
+    @Test
+    public void cannotMovePawnTest() {
+
+        ChessBoard board = new ChessBoard();
+        board.placePiece("King", "b", "A8");
+        board.placePiece("King", "w", "A1");
+        board.placePiece("Pawn", "w", "B1");
+        board.placePiece("Rook", "b", "H1");
+        board.placePiece("Pawn", "b", "B8");
+        board.placePiece("Rook", "w", "H8");
+
+        assertFalse("White pawn cannot move from B1 as King will be in check", board.movePiece("B1", "B2"));
+
+        board.changeTurn();
+
+        assertFalse("Black pawn cannont move from B8 as King will be in check", board.movePiece("B8", "B7"));
+
+    }
+
+
 }
